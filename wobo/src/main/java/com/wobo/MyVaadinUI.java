06a472fd24f3a92b4387677ff8ad3f54ce50002f
whit3hat88 <nils>
2014-12-10 10:07:16 +0100

    123

---
+++ wobo/src/main/java/com/wobo/MyVaadinUI.java
@@ -0,0 +1,36 @@
+package com.wobo;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI
+{
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.wobo.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+        
+        Button button = new Button("Intellij IDEA");
+        button.addClickListener(event -> layout.addComponent(new Label("MWI WoBo wird geil! :D :D")));
+        layout.addComponent(button);
+    }
+
+}
